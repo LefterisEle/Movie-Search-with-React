e275dd1daa8aa11fc5803512e0ca1a9750e54e56
LefterisEle <82530482+LefterisEle@users.noreply.github.com>
2022-10-28 16:10:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css'
+import SearchMovies from './SearchMovie';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className='title'>React Movie Search</h1>
+      <SearchMovies />
+    </div>
+  );
+}
+
+export default App;
